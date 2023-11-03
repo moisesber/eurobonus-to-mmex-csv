45fb102a1d93dc5a90210472b877413f7b54cea8
Moises Rodrigues <moisesr@spotify.com>
2023-11-03 16:40:14 +0100

    eurobonux Regex update and added more categories
     - regex divided into different sub regex
     - add several new categorios
     - add possibility to mark all expenses as vacation through vacation
       periods

--- app/euro2mmex_csv.py
+++ app/euro2mmex_csv.py
@@ -4,10 +4,20 @@ import re
 from datetime import datetime
 from eurobonus_categories import payee_to_labels, label_to_categories, list_invalid_payees
 
+other_expenses_string="Other Expenses"
+
 #date_pattern = '^\d{4}-\d{2}-\d{2}'
 date_pattern = '/^\d{4}\/\d{2}\/\d{2}'
 #valid_csv_line_pattern = '^\d{4}-\d{2}-\d{2},.+,[\d]+\.[\d]+,.+,(.+)?'
-valid_csv_line_pattern = '^\d{4}\/\d{2}\/\d{2},\d{4}\/\d{2}\/\d{2},["0-a ]+,["0-a ]+,(["0-a ]+)?,[\d]+,[0-9",]'
+#valid_csv_line_pattern = '^\d{4}\/\d{2}\/\d{2},\d{4}\/\d{2}\/\d{2},["0-a\*. ]+,["0-a ]+,(["0-a ]+)?,[\d]+,[0-9",]'
+#valid_csv_line_pattern = '^\d{4}\/\d{2}\/\d{2},\d{4}\/\d{2}\/\d{2},[0-9A-Za-z \*"\.]+,["0-a ]+,(["0-a ]+)?,[\d]+,[0-9",]'
+
+alphanumeric_word_with_special_chars_regex='[a-zA-Z0-9 -_äöåÄÖÅ"\*\.]+'
+date_regex='\d{4}\/\d{2}\/\d{2}'
+#valid_csv_line_pattern = '^\d{4}\/\d{2}\/\d{2},\d{4}\/\d{2}\/\d{2},[a-zA-Z0-9 _äöåÄÖÅ"\*\.]+,[\-"0-a ]+,([\-"0-a ]+)?,[\d]+,[\-0-9",]'
+valid_csv_line_pattern = '^{},{},{},{},([\-"0-a ]+)?,[\d]+,[\-0-9",]'.format(date_regex, date_regex, alphanumeric_word_with_special_chars_regex, alphanumeric_word_with_special_chars_regex)
+
+
 #valid_csv_line_pattern = '^\d{4}\/\d{2}\/\d{2},\d{1}'
 invoice_payment_pattern = 'BETALT BG DATUM'
 current_year = datetime.now().year
@@ -22,7 +32,9 @@ def get_category_from_payee(payee: str):
             category = label_to_categories()[label]
             return category[0], category[1]
 
-    return "Other Expenses", ""
+    #print("No payee found for |{}|".format(payee))
+
+    return other_expenses_string, ""
 
 def replace_commas_with_dots(value: str):
     return value.replace(",", ".")
@@ -30,31 +42,51 @@ def replace_commas_with_dots(value: str):
 def replace_slashes_with_dashes(string: str):
     return string.replace("/", "-")
 
+def get_date_object_from_string(date: str):
+    return datetime.strptime(date, '%Y-%m-%d').date()
+
+def is_date_between_period(date, start, end):
+    return date >= start and date <= end
+
+def is_current_data_vacation_date(date: datetime):
+    vacation_periods = [ 
+        { "from": "2023-06-25", "to": "2023-07-02" },
+        { "from": "2023-04-14", "to": "2023-04-19" }
+    ]
+    
+    for period in vacation_periods:
+        if is_date_between_period(date, get_date_object_from_string(period["from"]), get_date_object_from_string(period["to"])):
+            return True
+    return False
+
 list_of_csv_files = [
-    "invoices/jun-2022-eurobonus.csv",
-    "invoices/jul-2022-eurobonus.csv",
-    "invoices/aug-2022-eurobonus.csv",
-    "invoices/sep-2022-eurobonus.csv"
+#    "invoices/may-2022-eurobonus.csv",
+#    "invoices/jun-2022-eurobonus.csv",
+#    "invoices/jul-2022-eurobonus.csv",
+#    "invoices/aug-2022-eurobonus.csv",
+#    "invoices/sep-2022-eurobonus.csv"
+		"eurobonus-all-transactions-2023_01-01_10-31.csv",
+    "eurobonus-all-transactions-2022_12.csv"
 ]
 rows = []
 
 for csv_file in list_of_csv_files:
-				file = open(csv_file)
-				csvreader = csv.reader(file)
-				header = next(csvreader)
+        file = open(csv_file)
+        csvreader = csv.reader(file)
+        header = next(csvreader)
 
-				for row in csvreader:
-						row_full_line = ','.join(row)
-						is_valid_line = re.match(valid_csv_line_pattern, row_full_line)
-						if not is_valid_line:
-				#        print("full line |{}|".format(row_full_line))
-								print("Excluding row {}".format(row))
-								continue
-						else:
-								rows.append(row)
+        for row in csvreader:
+            row_full_line = ','.join(row)
+            is_valid_line = re.match(valid_csv_line_pattern, row_full_line)
+            if not is_valid_line:
+        #       print("full line |{}|".format(row_full_line))
+				#				print("Excluding row {}".format(row))
+                continue
+            else:
+                rows.append(row)
 #								print("Matched the regex {}".format(row))
 
-				file.close()
+        file.close()
 
 # ['04-30', '05-05', 'ÅRSAVGIFT', '', '', '', '1,495.00']
 # transforming into
@@ -75,6 +107,13 @@ for row in rows:
     if not is_payee_invalid:
         amount = replace_commas_with_dots(row[6])
         category, subcategory = get_category_from_payee(payee)
+
+        if category == other_expenses_string:
+            date_object = datetime.strptime(date, '%Y-%m-%d').date()
+            
+            if is_current_data_vacation_date(date_object):
+                category = "Vacation"
+                subcategory = "Travel"
 
         print("{},{},{},{},{}".format(date,payee,amount,category,subcategory))
 
